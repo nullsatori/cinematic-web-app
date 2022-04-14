9cdaa286221cc0d8cc9f558baf034ba846fedf99
satori <nullsatori@gmail.com>
2022-04-15 02:47:42 +0300

    added pfp upload in dashboard

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,43 +1,50 @@
-import React, {useEffect, useState} from "react";
-import {getAuth} from "firebase/auth";
-import {upload, useAuth} from "../firebase";
+import React, { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { upload, useAuth } from "../firebase";
 
 const Dashboard = () => {
   const currentUser = useAuth();
   const [photo, setPhoto] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [photoURL, setPhotoURL] = useState("https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png");
+  const [photoURL, setPhotoURL] = useState(
+    "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+  );
   const handleChange = (e) => {
-    if (e.target.files[0]){
+    if (e.target.files[0]) {
       setPhoto(e.target.files[0]);
     }
-  }
+  };
   const handleClickPicture = () => {
     upload(photo, currentUser, setLoading);
     console.log(currentUser);
-  }
-  useEffect( ()=> {
-    if (currentUser?.photoURL){
-      setPhotoURL(currentUser.photoURL)
+  };
+  useEffect(() => {
+    if (currentUser?.photoURL) {
+      setPhotoURL(currentUser.photoURL);
     }
-  }, [currentUser])
+  }, [currentUser]);
   return (
-    <div id='Dashboard'>
+    <div id="Dashboard">
       <h1>DASHBOARD</h1>
-        <div id="userOptions">
-          <div id="profile__picture">
-            <img src={photoURL} className='profile__picture__image' alt="user__profile__picture"/>
-            <input type="file" onChange={handleChange} />
-            <button disabled={loading || !photo} onClick={handleClickPicture}>Change profile picture</button>
-          </div>
-          <div id="user__info">
-            <div id="email">
-              <p>{currentUser?.email}</p>
-              <input type="text" onChange={handleChange} />
-
-            </div>
+      <div id="userOptions">
+        <div id="profile__picture">
+          <img
+            src={photoURL}
+            className="profile__picture__image"
+            alt="user__profile__picture"
+          />
+          <input type="file" onChange={handleChange} />
+          <button disabled={loading || !photo} onClick={handleClickPicture}>
+            Change profile picture
+          </button>
+        </div>
+        <div id="user__info">
+          <div id="email">
+            <p>{currentUser?.email}</p>
+            <input type="text" onChange={handleChange} />
           </div>
         </div>
+      </div>
     </div>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, onAuthStateChanged, updateProfile} from "firebase/auth";
+import { getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 const firebaseConfig = {
   apiKey: "AIzaSyAIiIF5ieAo8Cw9X51ZUD4Fx1olzcPaehY",
   authDomain: "cinematic-8b95f.firebaseapp.com",
@@ -18,21 +18,21 @@ export function useAuth() {
   const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, user => setCurrentUser(user));
+    const unsub = onAuthStateChanged(auth, (user) => setCurrentUser(user));
     return unsub;
-  }, [])
+  }, []);
 
   return currentUser;
 }
 export async function upload(file, currentUser, setLoading) {
-  const fileRef = ref(storage, currentUser.uid + '.png');
+  const fileRef = ref(storage, currentUser.uid + ".png");
 
   setLoading(true);
 
   const snapshot = await uploadBytes(fileRef, file);
   const photoURL = await getDownloadURL(fileRef);
 
-  updateProfile(currentUser, {photoURL});
+  updateProfile(currentUser, { photoURL });
 
   setLoading(false);
   alert("Uploaded file!");
